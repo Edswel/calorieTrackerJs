8651b6a028f1efbcee8936ad0e37886bca85798a
Edswel <franckini@yahoo.com>
2022-11-29 10:27:57 +0100

    Populate UI with update item functionality

--- app.js
+++ app.js
@@ -27,6 +27,16 @@ const StorageCtrl = (function () {
         items = JSON.parse(localStorage.getItem('items'));
       }
       return items;
+    },
+    updateItemStorage: function (updatedItem) {
+      let items = JSON.parse(localStorage.getItem('items'));
+      items.forEach((item, index) => {
+        if (updatedItem.id === item.id) {
+          items.splice(index, 1, updatedItem);
+        }
+      });
+      // Reset local storage
+      localStorage.setItem('items', JSON.stringify(items));
     }
   }
 })();
@@ -316,6 +326,8 @@ const App = (function (ItemCtrl, StorageCtrl, UICtrl) {
     const totalCalories = ItemCtrl.getTotalCalories();
     // Add total calories to UI
     UICtrl.showTotalCalories(totalCalories);
+    // Update local storage
+    StorageCtrl.updateItemStorage(updatedItem);
 
     UICtrl.clearEditState();
 
