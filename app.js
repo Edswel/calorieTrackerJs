ee1a87141ee025fe191a11a45e4aa0494e215872
Edswel <franckini@yahoo.com>
2022-11-29 08:54:49 +0100

    Add delete item functionality

--- app.js
+++ app.js
@@ -59,6 +59,16 @@ const ItemCtrl = (function () {
       });
       return found;
     },
+    deleteItem: function (id) {
+      // Get ids
+      const ids = data.items.map((item) => {
+        return item.id;
+      });
+      // Get index
+      const index = ids.indexOf(id);
+      // Remove item
+      data.items.splice(index, 1);
+    },
     setCurrentItem: function (item) {
       data.currentItem = item;
     },
@@ -198,6 +208,8 @@ const App = (function (ItemCtrl, UICtrl) {
     document.querySelector(UISelectors.itemList).addEventListener('click', itemEditClick);
     // Update item
     document.querySelector(UISelectors.updateBtn).addEventListener('click', itemUpdateSubmit);
+    // Delete item
+    document.querySelector(UISelectors.deleteBtn).addEventListener('click', itemDeleteSubmit);
     // Back button event
     document.querySelector(UISelectors.backBtn).addEventListener('click', UICtrl.clearEditState);
   }
@@ -251,6 +263,15 @@ const App = (function (ItemCtrl, UICtrl) {
     UICtrl.showTotalCalories(totalCalories);
 
     UICtrl.clearEditState();
+
+    e.preventDefault();
+  }
+  // Delete button event
+  const itemDeleteSubmit = function (e) {
+    // Get current item
+    const currentItem = ItemCtrl.getCurrentItem();
+    // Delete from data structure
+    ItemCtrl.deleteItem(currentItem.id);
 
     e.preventDefault();
   }
